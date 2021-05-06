0d4d744f326189d7eb07c7f32ac0e96686219cb4
vccAG52 <83731491+vccAG52@users.noreply.github.com>
2021-05-06 17:49:45 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+info.onCountdownEnd(function () {
+    game.showLongText("You ran out of time!", DialogLayout.Center)
+    game.over(false)
+})
+tiles.setTilemap(tilemap`level1`)
+let mySprite = sprites.create(img`
+    . . . . f f f f . . . . . 
+    . . f f f f f f f f . . . 
+    . f f f f f f c f f f . . 
+    f f f f f f c c f f f c . 
+    f f f c f f f f f f f c . 
+    c c c f f f e e f f c c . 
+    f f f f f e e f f c c f . 
+    f f f b f e e f b f f f . 
+    . f 4 1 f 4 4 f 1 4 f . . 
+    . f e 4 4 4 4 4 4 e f . . 
+    . f f f e e e e f f f . . 
+    f e f b 7 7 7 7 b f e f . 
+    e 4 f 7 7 7 7 7 7 f 4 e . 
+    e e f 6 6 6 6 6 6 f e e . 
+    . . . f f f f f f . . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+game.showLongText("I need to find my way to the blue smiley face.", DialogLayout.Full)
+info.startCountdown(120)
